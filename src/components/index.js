8a4d508043f43dddcc8e9a205530e0469509f70f
romit-gitcode <romitp.1401@gmail.com>
2023-02-23 18:52:33 +0530

    Focused Status Bar and FlatList

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import NFTCard from "./NFTCard";
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+
+export {
+    HomeHeader, NFTCard, FocusedStatusBar
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native'
+import React, { useState } from 'react'
+
+import { NFTData, COLORS } from "../../constants"
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components'
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1 }} >
+            <FocusedStatusBar background={COLORS.primary} />
+
+            <View style={{ flex: 1 }}>
+                <View style={{ zIndex: 0 }}>
+                    <FlatList
+                        data={NFTData}
+                        renderItem={({ item }) => {
+                            return <Text style={{ color: "black" }}>{item.name}</Text>
+                        }}
+                        keyExtractor={(item) => item.id}
+                        showsVerticalScrollIndicator={false}
+                        ListHeaderComponent={<HomeHeader />}
+                    />
+                </View>
+
+            </View>
+        </SafeAreaView>
+
     )
 }
 
