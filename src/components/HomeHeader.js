ea5d6933be9798f4f332c218c000d5e46a8c866e
romit-gitcode <romitp.1401@gmail.com>
2023-02-24 14:09:50 +0530

    Header attached

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 import { COLORS, FONTS, SIZES, assets } from '../../constants'
 
-const HomeHeader = () => {
+const HomeHeader = ({ onSearch }) => {
     return (
         <View style={{
             backgroundColor: COLORS.primary,
@@ -38,7 +38,36 @@ const HomeHeader = () => {
                     />
                 </View>
             </View>
+            <View style={{ marginVertical: SIZES.font }}>
+                <Text style={{ fontFamily: FONTS.regular, fontSize: SIZES.small, color: COLORS.white }}>
+                    Hello, Victoria 👋
+                </Text>
+                <Text style={{ fontFamily: FONTS.bold, fontSize: SIZES.large, color: COLORS.white, marginTop: SIZES.base / 2 }}>
+                    Let's find a masterpiece 🐉
+                </Text>
+            </View>
 
+            <View style={{ marginTop: SIZES.font }}>
+                <View style={{
+                    width: '100%',
+                    borderRadius: SIZES.font,
+                    backgroundColor: COLORS.gray,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    paddingHorizontal: SIZES.font,
+                    paddingVertical: SIZES.small - 2
+                }}>
+                    <Image
+                        source={assets.search}
+                        resizeMode='contain'
+                        style={{ width: 20, height: 20, marginRight: SIZES.base }}
+                    />
+                    <TextInput
+                        placeholder='Search NFTs'
+                        style={{ flex: 1 }}
+                        onChangeText={onSearch} />
+                </View>
+            </View>
         </View>
     )
 }
