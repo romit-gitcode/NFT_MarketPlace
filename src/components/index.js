74829fd89bdba6c96dd99cdd9eae08ac15b590f4
romit-gitcode <romitp.1401@gmail.com>
2023-02-24 16:39:35 +0530

    Details Completed

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,12 @@ import NFTCard from "./NFTCard";
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
 export {
-    HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton
+    HomeHeader, NFTCard, FocusedStatusBar, SubInfo,
+    CircleButton, RectButton,
+    DetailsDesc, DetailsBid
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,27 @@
-import { StyleSheet, Text, View, SafeAreaView, FlatList } from 'react-native'
+import { StyleSheet, View, SafeAreaView, FlatList } from 'react-native'
 import React, { useState } from 'react'
 
 import { NFTData, COLORS } from "../../constants"
 import { NFTCard, HomeHeader, FocusedStatusBar } from '../components'
 
+
 const Home = () => {
+    const [nftData, setNftData] = useState(NFTData);
+
+    const handleSearch = (value) => {
+        if (!value.length) return setNftData(NFTData);
+
+        const filteredData = NFTData.filter((item) =>
+            item.name.toLowerCase().includes(value.toLowerCase()));
+
+        if (!filteredData.length) {
+            setNftData(NFTData);
+        } else {
+            setNftData(filteredData);
+        }
+
+    }
+
     return (
         <SafeAreaView style={{ flex: 1 }} >
             <FocusedStatusBar backgroundColor={COLORS.primary} />
@@ -12,13 +29,13 @@ const Home = () => {
             <View style={{ flex: 1 }}>
                 <View style={{ zIndex: 0 }}>
                     <FlatList
-                        data={NFTData}
+                        data={nftData}
                         renderItem={({ item }) => {
                             return <NFTCard data={item}></NFTCard>
                         }}
                         keyExtractor={(item) => item.id}
                         showsVerticalScrollIndicator={false}
-                        ListHeaderComponent={<HomeHeader />}
+                        ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
                     />
                 </View>
                 <View style={{
@@ -30,7 +47,7 @@ const Home = () => {
                     zIndex: -1,
                 }}>
                     <View style={{ height: 300, backgroundColor: COLORS.primary }} />
-                    <View style={{ flex: 1, backgroundColor: COLORS.gray }} />
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }} />
                 </View>
             </View>
         </SafeAreaView>
